3a47e6765ec51d8e63371c68c1b02964f5888428
Lrakulka <olecsandr32@gmail.com>
2022-08-07 14:01:46 +0200

    2022-08-07

--- src/FurnishedParserFilter.py
+++ src/FurnishedParserFilter.py
@@ -50,7 +50,7 @@ def priceMeterSort(e):
 
 
 def main():
-    map = readFromFile("./2022-07-31")
+    map = readFromFile("./2022-08-07")
     myCheckIn = datetime.strptime("2022-12-15", DATE_FORMAT)
     residencesCheapest = map.copy()
     residencesCheapest.sort(reverse=False, key=priceSort)
